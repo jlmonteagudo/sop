d064afb844b8046ca4546955a110ab13ac387777
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2014-09-22 22:52:56 +0200

    chapter6

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/ng-table/ng-table.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -25,8 +26,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
-			]
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/ng-table/ng-table.min.js'
+				]
 		},
 		css: [
 			'public/modules/**/css/*.css'
